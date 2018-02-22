40f5411fb37f3a830e08578a0f4857690ab7b16e
MikanHako1024 <MikanHako@outlook.com>
2018-02-22 20:59:24 +0800

    增加拖入文件解题功能

--- SolveSuDoKu.cpp
+++ SolveSuDoKu.cpp
@@ -5,6 +5,7 @@ SolveSuDoKu
 
 
 #include <stdio.h>
+#include <stdlib.h>
 
 // 大格 高宽
 #define H (3)
@@ -12,6 +13,7 @@ SolveSuDoKu
 // 棋盘 边长
 #define LEN (9)
 
+/*
 int map[LEN][LEN]={ 
 {0, 0, 5,   0, 0, 0,   0, 0, 0}, 
 {0, 0, 0,   0, 6, 5,   8, 0, 0}, 
@@ -24,7 +26,9 @@ int map[LEN][LEN]={
 {3, 0, 7,   0, 5, 0,   4, 9, 0}, 
 {0, 0, 1,   6, 4, 0,   0, 0, 0}, 
 {0, 0, 0,   0, 0, 0,   1, 0, 0} };
-//int solvemap[LEN][LEN];
+*/
+int map[LEN][LEN];
+
 
 int check(int x, int y, int num)
 	// 检查 [x][y]格是否可以填入num 
@@ -89,6 +93,14 @@ int dfs(int x, int y)
 	return 0;
 }
 
+
+void ScanMap(int *arr, int len)
+{
+	int z, max=len*len;
+	for(z=0; z<max; z++)
+		scanf("%d", arr+z);
+}
+
 void PrintMap(int *arr, int len)
 {
 	int z, max=len*len;
@@ -99,27 +111,108 @@ void PrintMap(int *arr, int len)
 	}
 }
 
-void ScanMap(int *arr, int len)
+
+void ScanMapF(char *FName, int *arr, int len)
 {
+	FILE *fp=NULL;
+	if( (fp=fopen(FName, "r"))==NULL )
+	{
+		printf("打开 %s 失败！\n");
+		system("pause");
+		exit(0); 
+	}
+	
 	int z, max=len*len;
 	for(z=0; z<max; z++)
-		scanf("%d", arr+z);
+		fscanf(fp, "%d", arr+z);
+	
+	fclose(fp);
 }
 
+void PrintMapF(char *FName, int *arr, int len)
+{
+	FILE *fp=NULL;
+	if( (fp=fopen(FName, "a"))==NULL )
+	{
+		printf("打开 %s 失败！\n");
+		system("pause");
+		exit(0); 
+	}
+	
+	int z, max=len*len;
+	for(z=0; z<max; z++)
+	{
+		fprintf(fp, "%d ", *(arr+z));
+		if( (z+1)%len==0 )fprintf(fp, "\n");
+	}
+	
+	fclose(fp);
+}
+
+void PrintStrF(char *FName, const char *str)
+{
+	FILE *fp=NULL;
+	if( (fp=fopen(FName, "a"))==NULL )
+	{
+		printf("打开 %s 失败！\n");
+		//system("pause");
+		exit(0); 
+	}
+	
+	fprintf(fp, "%s", str);
+	
+	fclose(fp);
+}
 
 #define TestMod 0
-	// 0 ：输入模式
+	// 0 ：输入/拖入模式
 	// 1 : 预设模式 
 
 int main(int argc, char *argv[])
 {
-	if( TestMod==0 )ScanMap(&map[0][0], LEN);
-	else PrintMap(&map[0][0], LEN);
-	printf("Answer：\n");
+	if( TestMod==1 )
+	{
+		printf("Question：\n"); 
+		PrintMap(&map[0][0], LEN);
+		printf("\nAnswer：\n");
 	
-	if( dfs(0, 0)==0 )printf("无解\n");
-	else PrintMap(&map[0][0], LEN);
+		if( dfs(0, 0)==0 )printf("无解\n");
+		else PrintMap(&map[0][0], LEN);
+	}
+	else
+	{
+		if( argc>1 )
+		{
+			ScanMapF(argv[1], &map[0][0], LEN);
+			printf("Question：\n"); 
+			PrintMap(&map[0][0], LEN);
+			
+			PrintStrF(argv[1], "\nAnswer：\n");
+			printf("\nAnswer：\n");
+			
+			if( dfs(0, 0)==0 )
+			{
+				PrintStrF(argv[1], "无解\n");
+				printf("无解\n");
+			}
+			else
+			{
+				PrintMapF(argv[1], &map[0][0], LEN);
+				PrintMap(&map[0][0], LEN);
+			}
+		}
+		else
+		{
+			printf("Question：\n"); 
+			ScanMap(&map[0][0], LEN);
+			printf("Answer：\n");
+			
+			if( dfs(0, 0)==0 )printf("无解\n");
+			else PrintMap(&map[0][0], LEN);
+		}
+	}
 	
+	system("pause");
 	return 0;
 }
 
